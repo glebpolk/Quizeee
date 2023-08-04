fac3c9c1e0ca1005ec525e084d9f8b900eb066fd
Varvara Groznykh <varenkoluchka@gmail.com>
2023-08-04 11:26:34 +0300

    registrationForm

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,1 +1,21 @@
-console.log("Hey");
+const formReg = document.querySelector('#form-reg')
+
+formReg.addEventListener('submit', async(e)=>{
+e.preventDefault();
+const{name} = e.target;
+const res = await fetch('/',{
+method: 'POST',
+headers:{
+  'Content-type': 'application/json',
+},
+body: JSON.stringify({
+name: name.value,
+}),
+});
+const data = await res.json();
+  if (data.message !== 'ok') {
+    alert(data.message);
+  }else{
+    window.location.assign('/themes')
+  }
+})
